1966afb1896e66b4ef1c009a6a21b44300fa05fd
aliwalsh1 <awalsh97@icloud.com>
2017-12-17 13:18:07 +0000

    First Commit

---
+++ src/Messages.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Messages extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Messages</h2>
+                <p>Messages between members</p>
+            </div>
+        );
+    }
+}
+
+export default Messages
---
+++ src/Results.js
@@ -0,0 +1,14 @@
+import React, { Component } from "react";
+
+class Results extends React.Component {
+    render() {
+        return (
+            <div>
+                <h2>Results</h2>
+                <p>Results of past tournaments</p>
+            </div>
+        );
+    }
+}
+
+export default Results
